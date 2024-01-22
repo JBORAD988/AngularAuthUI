e3fe025d286d40cd3c312580d729f5ec753ad77e
jayborad <co-20-081@rctiahmedabad.gujgov.edu.in>
2024-01-22 18:49:20 +0530

    Update-7.2_Implement-JWT_token_in_Angular

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../services/auth.service";
+import {ApiService} from "../../services/api.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -8,10 +9,14 @@ import {AuthService} from "../../services/auth.service";
 })
 export class DashboardComponent implements OnInit{
 
-  constructor(private auth: AuthService) {
+  public users:any = [];
+  constructor(private auth: AuthService, private api: ApiService) {
   }
 
   ngOnInit() {
+    this.api.getUsers().subscribe(x=>{
+      this.users = x;
+    })
 
   }
 
